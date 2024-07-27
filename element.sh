a83b09cb09073a11aa889b4981dfdb7b7595cb15
hezirel <80560775+hezirel@users.noreply.github.com>
2024-07-27 10:51:39 +0200

    feat: basic args check loop

--- element.sh
+++ element.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+# If no argument or more than one is provided, return an error message
+if [ "$#" -ne 1 ]; then
+    echo "Please provide an element as an argument."
+    exit
+    fi
